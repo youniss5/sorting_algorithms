67eb6dcc325308270e240ea6a08f11f0b35a39e1
youniss5 <ahmedhyouniss@gmail.com>
2023-10-23 17:26:09 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,25 +6,25 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-        int tem;
-        size_t j, k;
+int tem;
+size_t j, k;
 
-        if (!array || !size)
-                return;
+if (!array || !size)
+return;
 
-        j = 0;
-        while (j < size)
-        {
-                for (k = 0; k < size - 1; k++)
-                {
-                        if (array[k] > array[k + 1])
-                        {
-                                tem = array[k];
-                                array[k] = array[k + 1];
-                                array[k + 1] = tem;
-                                print_array(array, size);
-                        }
-                }
-                j++;
-        }
+j = 0;
+while (j < size)
+{
+for (k = 0; k < size - 1; k++)
+{
+if (array[k] > array[k + 1])
+{
+tem = array[k];
+array[k] = array[k + 1];
+array[k + 1] = tem;
+print_array(array, size);
+}
+}
+j++;
+}
 }
